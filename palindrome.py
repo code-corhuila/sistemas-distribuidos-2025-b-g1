b2dc59da7f78ea89db759f9f623766732f6b2189
VictorAndres123 <132307617+VictorAndres123@users.noreply.github.com>
2025-08-13 20:03:33 -0500

    Mejorar algoritmo para ignorar mayúsculas y espacios (HU-2)

--- palindrome.py
+++ palindrome.py
@@ -1,7 +1,8 @@
-def is_palindrome_basic(text):
-    return text == text[::-1]
+def is_palindrome_optimized(text):
+    # Elimina espacios y convierte todo a minúsculas
+    clean_text = ''.join(text.split()).lower()
+    return clean_text == clean_text[::-1]
 
 if __name__ == "__main__":
     entrada = input("Introduce una palabra o frase: ")
-    print("¿Es palíndromo?", is_palindrome_basic(entrada))
-
+    print("¿Es palíndromo?", is_palindrome_optimized(entrada))
